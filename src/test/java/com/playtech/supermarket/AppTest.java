4f6cfce438637febf83f3218342145fb693d64b7
Antonio Gomez <>
2020-07-29 11:03:53 +0100

    First test case done

--- src/test/java/com/playtech/supermarket/AppTest.java
+++ src/test/java/com/playtech/supermarket/AppTest.java
@@ -1,38 +1,48 @@
 package com.playtech.supermarket;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    @Test
+    public void testOutputStream()
     {
-        assertTrue( true );
+        System.out.print("hello");
+        assertEquals("hello", outContent.toString());
+    }
+
+    @Test
+    public void main_BuyAppleMilkBread_ShouldOutputOk()
+    {
+        String expectedOutput =
+                        "Subtotal:        £3.10\n" +
+                        "Apples 10% off:    -10p\n" +
+                        "Total:           £3.00\n";
+
+        App.main(new String[]{"Apple", "Milk", "Bread"});
+        assertEquals("expectedOutput", outContent.toString());
     }
 }
